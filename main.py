8fda729fb5cb1add2cd18d5718d07cb782ddcbb8
Jamorobo <github@jamorobo.com>
2019-10-06 00:32:54 +0100

    added csv reader v1

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python3
+import sys
 import csv
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-location",
-                    "-l",
-                    dest="loc",
+parser.add_argument("PATH",
+                    type=str,
                     help="Location of csv file")
 args = parser.parse_args()
-print(args)
 
 
-def load_csv(file_loc):
-    csv_dict = csv.DictReader(file_loc, delimiter=',')
-    return csv_dict
+def csv_to_dict(file_loc):
+    with open(file_loc, mode="r") as csv_file:
+        csv_dict = csv.reader(csv_file, delimiter=',')
+        line = 0
+        headers = []
+        main_list = []
+        for row in csv_dict:
+            if line == 0:
+                headers.extend(row)
+                print(headers)
+            else:
+                temp_dict = {}
+                for item in row:
+                    print(item)
+                    temp_dict[headers[row.index(item)]] = item
+                main_list.append(temp_dict)
+            line += 1
+        print(main_list)
 
 
 
 
+
+
+if __name__ == "__main__":
+    csv_to_dict(args.PATH)
